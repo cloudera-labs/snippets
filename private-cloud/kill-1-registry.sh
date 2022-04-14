cf44a67ad95d9e511ce2107bf2c976c09f42d518
Travis Campbell <tcampbell@cloudera.com>
2022-04-14 16:02:41 -0500

    Add in shell scripts for cleaning up failed private-cloud (ECS) deployments so we can later attempt a reinstall
    Signed-off-by: Travis Campbell <tcampbell@cloudera.com>
    Signed-off-by: Travis Campbell <hcoyote@ghostar.org>

---
+++ private-cloud/kill-1-registry.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Copyright 2022 Cloudera, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+input="target-hosts.txt"
+
+#
+# modify these values as appropriate for your local environment
+#
+priv_key="~/systest"
+sudo_user="systest"
+
+# stop the registry container and remove it
+while read -r -u10 host;
+do echo '"'Trying..${host}'"';
+ssh -i ${priv_key} -o StrictHostKeyChecking=no ${sudo_user}@${host} "
+sudo /opt/cloudera/parcels/ECS/docker/docker container stop registry;
+sudo /opt/cloudera/parcels/ECS/docker/docker container rm -v registry;
+sudo /opt/cloudera/parcels/ECS/docker/docker image rm registry:2";
+done 10< "$input"
+
+echo "Registry killed, now shut down ECS service"
